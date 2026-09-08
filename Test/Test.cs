83f4370a65cc3ce78c663da1d08e1caeb8c8754a
Samuel <samuel.smlxdesign@gmail.com>
2026-09-08 21:19:18 +0200

    Refactor(test): remove reduntant identifier

--- Test/Test.cs
+++ Test/Test.cs
@@ -8,7 +8,7 @@ public sealed class Test
     [TestMethod]
     public void TestMethod1()
     {
-        var result = Parkeringsautomaten.Program.CalculateCost(12, Program.Day.Monday);
+        var result = Program.CalculateCost(12, Program.Day.Monday);
         var expected = 0;
         Assert.AreEqual(expected, result);
     }
@@ -16,7 +16,7 @@ public sealed class Test
     [TestMethod]
     public void TestMethod2()
     {
-        var result = Parkeringsautomaten.Program.CalculateCost(20, Program.Day.Tuesday);
+        var result = Program.CalculateCost(20, Program.Day.Tuesday);
         var expected = 5;
         Assert.AreEqual(expected, result);
     }
@@ -24,7 +24,7 @@ public sealed class Test
     [TestMethod]
     public void TestMethod3()
     {
-        var result = Parkeringsautomaten.Program.CalculateCost(35, Program.Day.Wednesday);
+        var result = Program.CalculateCost(35, Program.Day.Wednesday);
         var expected = 10;
         Assert.AreEqual(expected, result);
     }
@@ -32,7 +32,7 @@ public sealed class Test
     [TestMethod]
     public void TestMethod4()
     {
-        var result = Parkeringsautomaten.Program.CalculateCost(35, Program.Day.Saturday);
+        var result = Program.CalculateCost(35, Program.Day.Saturday);
         var expected = 5;
         Assert.AreEqual(expected, result);
     }
@@ -40,7 +40,7 @@ public sealed class Test
     [TestMethod]
     public void TestMethod5()
     {
-        var result = Parkeringsautomaten.Program.CalculateCost(1000, Program.Day.Sunday);
+        var result = Program.CalculateCost(1000, Program.Day.Sunday);
         var expected = 150;
         Assert.AreEqual(expected, result);
     }
