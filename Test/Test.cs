e7a5db5d5a271cb6e910cfad2df3a12190547a08
Samuel <samuel.pettersson1@edu.varberg.se>
2026-09-08 15:48:10 +0200

    Add unit testing

---
+++ Test/Test.cs
@@ -0,0 +1,45 @@
+﻿namespace Test;
+
+[TestClass]
+public sealed class Test
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var result = Parkeringsautomaten.Program.CalculateCost(12, "måndag");
+        var expected = 0;
+        Assert.AreEqual(expected, result);
+    }
+    
+    [TestMethod]
+    public void TestMethod2()
+    {
+        var result = Parkeringsautomaten.Program.CalculateCost(20, "tisdag");
+        var expected = 5;
+        Assert.AreEqual(expected, result);
+    }
+    
+    [TestMethod]
+    public void TestMethod3()
+    {
+        var result = Parkeringsautomaten.Program.CalculateCost(35, "onsdag");
+        var expected = 10;
+        Assert.AreEqual(expected, result);
+    }
+    
+    [TestMethod]
+    public void TestMethod4()
+    {
+        var result = Parkeringsautomaten.Program.CalculateCost(35, "lördag");
+        var expected = 5;
+        Assert.AreEqual(expected, result);
+    }
+    
+    [TestMethod]
+    public void TestMethod5()
+    {
+        var result = Parkeringsautomaten.Program.CalculateCost(1000, "söndag");
+        var expected = 150;
+        Assert.AreEqual(expected, result);
+    }
+}
