0561636743aca9d27c5c49c9f1ea5e4a31f994ff
Samuel <samuel.smlxdesign@gmail.com>
2026-09-22 09:07:08 +0200

    Rename tests and test file

--- Parkeringsautomaten.Test/Cost.cs
+++ Parkeringsautomaten.Test/Cost.cs
@@ -1,42 +1,50 @@
 ﻿namespace Parkeringsautomaten.Test;
 
 [TestClass]
-public sealed class Test
+public sealed class Cost
 {
     [TestMethod]
-    public void TestMethod1()
+    public void NoDuration()
+    {
+        var result = Lib.Cost.Calculate(0, DayOfWeek.Monday);
+        var expected = 0;
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void ShortDuration()
     {
         var result = Lib.Cost.Calculate(12, DayOfWeek.Monday);
         var expected = 0;
         Assert.AreEqual(expected, result);
     }
-    
+
     [TestMethod]
-    public void TestMethod2()
+    public void Normal1()
     {
         var result = Lib.Cost.Calculate(20, DayOfWeek.Tuesday);
         var expected = 5;
         Assert.AreEqual(expected, result);
     }
-    
+
     [TestMethod]
-    public void TestMethod3()
+    public void Normal2()
     {
         var result = Lib.Cost.Calculate(35, DayOfWeek.Wednesday);
         var expected = 10;
         Assert.AreEqual(expected, result);
     }
-    
+
     [TestMethod]
-    public void TestMethod4()
+    public void Weekend()
     {
         var result = Lib.Cost.Calculate(35, DayOfWeek.Saturday);
         var expected = 5;
         Assert.AreEqual(expected, result);
     }
-    
+
     [TestMethod]
-    public void TestMethod5()
+    public void LongDuration()
     {
         var result = Lib.Cost.Calculate(1000, DayOfWeek.Sunday);
         var expected = 150;
